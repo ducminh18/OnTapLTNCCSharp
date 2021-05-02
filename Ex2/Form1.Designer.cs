0fe7c17f281e8d54e0a244a486243b7e84d8bd90
pdong2001 <79776591+pdong2001@users.noreply.github.com>
2021-05-02 11:11:12 +0700

    Completed Ex 2

--- Ex2/Form1.Designer.cs
+++ Ex2/Form1.Designer.cs
@@ -30,11 +30,17 @@ namespace Ex2
         private void InitializeComponent()
         {
             this.label1 = new System.Windows.Forms.Label();
-            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.dgvMain = new System.Windows.Forms.DataGridView();
+            this.dgCl_ID = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.dgCL_Name = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.dgCl_Class = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.dgCl_Birth = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.dgCl_Sex = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.dgCl_Country = new System.Windows.Forms.DataGridViewTextBoxColumn();
             this.label2 = new System.Windows.Forms.Label();
             this.btnSearch = new System.Windows.Forms.Button();
             this.txtSearch = new System.Windows.Forms.TextBox();
-            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dgvMain)).BeginInit();
             this.SuspendLayout();
             // 
             // label1
@@ -49,13 +55,68 @@ namespace Ex2
             // 
             // dataGridView1
             // 
-            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
-            this.dataGridView1.Location = new System.Drawing.Point(7, 66);
-            this.dataGridView1.Name = "dataGridView1";
-            this.dataGridView1.RowHeadersWidth = 51;
-            this.dataGridView1.RowTemplate.Height = 24;
-            this.dataGridView1.Size = new System.Drawing.Size(949, 379);
-            this.dataGridView1.TabIndex = 3;
+            this.dgvMain.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dgvMain.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.dgCl_ID,
+            this.dgCL_Name,
+            this.dgCl_Class,
+            this.dgCl_Birth,
+            this.dgCl_Sex,
+            this.dgCl_Country});
+            this.dgvMain.Location = new System.Drawing.Point(7, 67);
+            this.dgvMain.MultiSelect = false;
+            this.dgvMain.Name = "dataGridView1";
+            this.dgvMain.RowHeadersWidth = 51;
+            this.dgvMain.RowTemplate.Height = 24;
+            this.dgvMain.SelectionMode = System.Windows.Forms.DataGridViewSelectionMode.FullRowSelect;
+            this.dgvMain.Size = new System.Drawing.Size(949, 379);
+            this.dgvMain.TabIndex = 3;
+            this.dgvMain.CellClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.dgvMain_CellClick);
+            // 
+            // dgCl_ID
+            // 
+            this.dgCl_ID.AutoSizeMode = System.Windows.Forms.DataGridViewAutoSizeColumnMode.AllCells;
+            this.dgCl_ID.HeaderText = "Mã SV";
+            this.dgCl_ID.MinimumWidth = 6;
+            this.dgCl_ID.Name = "dgCl_ID";
+            this.dgCl_ID.Width = 78;
+            // 
+            // dgCL_Name
+            // 
+            this.dgCL_Name.AutoSizeMode = System.Windows.Forms.DataGridViewAutoSizeColumnMode.Fill;
+            this.dgCL_Name.HeaderText = "Tên SV";
+            this.dgCL_Name.MinimumWidth = 6;
+            this.dgCL_Name.Name = "dgCL_Name";
+            // 
+            // dgCl_Class
+            // 
+            this.dgCl_Class.AutoSizeMode = System.Windows.Forms.DataGridViewAutoSizeColumnMode.AllCells;
+            this.dgCl_Class.HeaderText = "Lớp";
+            this.dgCl_Class.MinimumWidth = 6;
+            this.dgCl_Class.Name = "dgCl_Class";
+            this.dgCl_Class.Width = 61;
+            // 
+            // dgCl_Birth
+            // 
+            this.dgCl_Birth.AutoSizeMode = System.Windows.Forms.DataGridViewAutoSizeColumnMode.AllCells;
+            this.dgCl_Birth.HeaderText = "Ngày sinh";
+            this.dgCl_Birth.MinimumWidth = 6;
+            this.dgCl_Birth.Name = "dgCl_Birth";
+            // 
+            // dgCl_Sex
+            // 
+            this.dgCl_Sex.AutoSizeMode = System.Windows.Forms.DataGridViewAutoSizeColumnMode.AllCells;
+            this.dgCl_Sex.HeaderText = "Giới tính";
+            this.dgCl_Sex.MinimumWidth = 6;
+            this.dgCl_Sex.Name = "dgCl_Sex";
+            this.dgCl_Sex.Width = 89;
+            // 
+            // dgCl_Country
+            // 
+            this.dgCl_Country.AutoSizeMode = System.Windows.Forms.DataGridViewAutoSizeColumnMode.Fill;
+            this.dgCl_Country.HeaderText = "Quê quán";
+            this.dgCl_Country.MinimumWidth = 6;
+            this.dgCl_Country.Name = "dgCl_Country";
             // 
             // label2
             // 
@@ -75,6 +136,7 @@ namespace Ex2
             this.btnSearch.TabIndex = 5;
             this.btnSearch.Text = "Tìm";
             this.btnSearch.UseVisualStyleBackColor = true;
+            this.btnSearch.Click += new System.EventHandler(this.btnSearch_Click);
             // 
             // txtSearch
             // 
@@ -82,6 +144,7 @@ namespace Ex2
             this.txtSearch.Name = "txtSearch";
             this.txtSearch.Size = new System.Drawing.Size(100, 22);
             this.txtSearch.TabIndex = 6;
+            this.txtSearch.Text = "@ALL";
             // 
             // Form1
             // 
@@ -91,12 +154,12 @@ namespace Ex2
             this.Controls.Add(this.txtSearch);
             this.Controls.Add(this.btnSearch);
             this.Controls.Add(this.label2);
-            this.Controls.Add(this.dataGridView1);
+            this.Controls.Add(this.dgvMain);
             this.Controls.Add(this.label1);
             this.Name = "Form1";
             this.Text = "Form1";
             this.Load += new System.EventHandler(this.Form1_Load);
-            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dgvMain)).EndInit();
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -105,10 +168,16 @@ namespace Ex2
         #endregion
 
         private System.Windows.Forms.Label label1;
-        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.DataGridView dgvMain;
         private System.Windows.Forms.Label label2;
         private System.Windows.Forms.Button btnSearch;
         private System.Windows.Forms.TextBox txtSearch;
+        private System.Windows.Forms.DataGridViewTextBoxColumn dgCl_ID;
+        private System.Windows.Forms.DataGridViewTextBoxColumn dgCL_Name;
+        private System.Windows.Forms.DataGridViewTextBoxColumn dgCl_Class;
+        private System.Windows.Forms.DataGridViewTextBoxColumn dgCl_Birth;
+        private System.Windows.Forms.DataGridViewTextBoxColumn dgCl_Sex;
+        private System.Windows.Forms.DataGridViewTextBoxColumn dgCl_Country;
     }
 }
 
--- Ex2/Form1.cs
+++ Ex2/Form1.cs
@@ -22,5 +22,31 @@ namespace Ex2
         {
            
         }
+
+        private void btnSearch_Click(object sender, EventArgs e)
+        {
+            StreamReader strRead = new StreamReader(Environment.CurrentDirectory + "\\CongNgheNET.txt");
+            string Search = txtSearch.Text;
+            dgvMain.Rows.Clear();
+            while (!strRead.EndOfStream)
+            {
+                string _str = strRead.ReadLine();
+
+                string[] _array = _str.Split(',');
+                if (_array[2].ToLower().Contains(Search.ToLower()) || Search == "@ALL")
+                {
+                    dgvMain.Rows.Add(_array);
+                }
+            }
+        }
+
+        private void dgvMain_CellClick(object sender, DataGridViewCellEventArgs e)
+        {
+            DataGridViewCellCollection Cell = dgvMain.SelectedRows[0].Cells;
+            string Result = $"Mã SV : {Cell[0].Value} - Tên SV: {Cell[1].Value}\nLớp : {Cell[2].Value} - Ngày sinh : {Cell[3].Value}" +
+                $"\nGiới tính : {Cell[4].Value} - Quê quán : {Cell[5].Value}";
+                MessageBox.Show(Result, "Chi tiết") ;
+
+        }
     }
 }
